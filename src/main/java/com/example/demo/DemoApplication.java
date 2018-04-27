aa7433c37267701c4a0ee79a8a385fe7062b8f93
dirkdeyne <dirkdeyne@gmail.com>
2018-04-27 15:14:30 +0200

    add RestController

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,9 @@
 package com.example.demo;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -13,7 +17,12 @@ public class DemoApplication  implements CommandLineRunner{
   
   @Override
   public void run(String... args) throws Exception {
-     personRepo.save(new Person("Deyne","Dirk"));
+     Set<Person> people = Stream.of("Deyne,Dirk","Devos,Eric","Mahsum,Mahsum")
+           .map(s -> s.split(","))
+           .map(a -> new Person(a[0], a[1]))
+           .collect(Collectors.toSet());
+           
+     personRepo.saveAll(people);
      
      personRepo.findByNameContains("Deyn").forEach(n -> System.err.println(n.getName()));
   }
